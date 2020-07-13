1d1a544b35ee511575b56f7012f8dd5ceab09e4a
rieflint <57578036+rieflint@users.noreply.github.com>
2020-07-13 14:33:10 -0400

    Section 5

--- script.js
+++ script.js
@@ -237,7 +237,7 @@
 // john.presentation("formal", "morning");
 // john.presentation.call(emily, "friendly", "afternoon");
 
-// // john.presentation.apply(emily, ["frienldy", "afternoon"])
+// // john.presentation.apply(emily, ["friendly", "afternoon"])
 
 // var johnFriendly = john.presentation.bind(john, "friendly");
 
@@ -247,25 +247,105 @@
 // var emilyFormal = john.presentation.bind(emily, "formal");
 // emilyFormal("afternoon");
 
-var years = [1990, 1965, 1937, 2005, 1998];
+// var years = [1990, 1965, 1937, 2005, 1998];
 
-function arrayCalc(arr, fn) {
-  var arrRes = [];
-  for (var i = 0; i < arr.length; i++) {
-    arrRes.push(fn(arr[i]));
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+
+// function calculateAge(el) {
+//   return 2016 - el;
+// }
+
+// function isFullAge(limit, el) {
+//   return el >= limit;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+// console.log(ages);
+// console.log(fullJapan);
+
+//
+
+(function () {
+  function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
   }
-  return arrRes;
-}
 
-function calculateAge(el) {
-  return 2016 - el;
-}
+  Question.prototype.displayQuestion = function () {
+    console.log(this.question);
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ": " + this.answers[i]);
+    }
+  };
 
-function isFullAge(limit, el) {
-  return el >= limit;
-}
+  Question.prototype.checkAnswer = function (ans, callback) {
+    var sc;
+    if (ans === this.correct) {
+      console.log("Correct answer!");
+      sc = callback(true);
+    } else {
+      console.log("Wrong answer.  Try again. :(");
+      sc = callback(false);
+    }
+    this.displayScore(sc);
+  };
 
-var ages = arrayCalc(years, calculateAge);
-var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
-console.log(ages);
-console.log(fullJapan);
+  Question.prototype.displayScore = function (score) {
+    console.log("Your current score is: " + score);
+    console.log("-----------------------");
+  };
+
+  var q1 = new Question(
+    "Is JavaScript the coolest programming language in the world?",
+    ["Yes", "No"],
+    0
+  );
+  var q2 = new Question(
+    "What is the name of this course's teacher?",
+    ["John", "Michael", "Jonas"],
+    2
+  );
+  var q3 = new Question(
+    "What best describes coding?",
+    ["Boring", "Hard", "Fun", "Tedious"],
+    2
+  );
+
+  var questions = [q1, q2, q3];
+
+  function score() {
+    var sc = 0;
+    return function (correct) {
+      if (correct) {
+        sc++;
+      }
+      return sc;
+    };
+  }
+
+  var keepScore = score();
+
+  function nextQuestion() {
+    var n = Math.floor(Math.random() * questions.length);
+
+    questions[n].displayQuestion();
+
+    var answer = prompt("Please select the correct answer.");
+
+    if (answer !== "exit") {
+      questions[n].checkAnswer(parseInt(answer), keepScore);
+
+      nextQuestion();
+    }
+  }
+
+  nextQuestion();
+})();
